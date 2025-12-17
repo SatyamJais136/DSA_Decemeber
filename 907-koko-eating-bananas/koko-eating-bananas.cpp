33ed38221c392130028e0d1e9bd14f9f43aef513
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2025-12-18 01:09:58 +0530

    Time: 5 ms (81.42%) | Memory: 22.9 MB (43.78%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool check(int mid,vector<int>& piles, int h){
-    int count = 0;
-    int n = piles.size();
-    for(int i = 0; i < n; i++){
+        int n = piles.size();
+        int count = 0;
+        for(int i = 0; i <n ; i++){
+            if(count > h) return false;
+            if(mid >= piles[i]) count++;
+            else if(piles[i] % mid == 0) count += piles[i]/mid;
+            else count += piles[i]/mid + 1;
+        }
         if(count > h) return false;
-        if(mid >= piles[i]) count++;
-        else if(piles[i] % mid == 0) count += piles[i]/mid;
-        else count += (piles[i]/mid) + 1;
-    }
-    if(count > h) return false;
-    else return true;
+        return true;
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
         int l = 1;
         int mx = -1;
-        for(int i = 0; i < n; i++){
-            mx = max(mx, piles[i]);
+        for(int pile: piles){
+            mx = max(mx,pile);
         }
         int hi = mx;
         int ans = 0;
@@ -27,9 +27,13 @@ public:
             if(check(mid, piles, h)){
                 ans = mid;
                 hi = mid -1;
+                // if(mid >= h){
+                //     hi = mid -1;
+                // }
             }
-            else l = mid + 1;
+            else l = mid+1;
         }
+        
         return ans;
     }
 };
